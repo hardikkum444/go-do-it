dd2ef7c18d8124bec3c59d108e16c72a53f35e99
hardikkum444 <hardikkumawat444@gmail.com>
2024-11-10 12:50:55 +0530

    delete added

--- todo.go
+++ todo.go
@@ -4,7 +4,7 @@ import(
 
     "github.com/hardikkum444/go-do-it/cmd"
     "time"
-    
+    "errors"
 )
 
 type Todo struct{
@@ -18,7 +18,7 @@ type Todos []Todo
 
 var todos Todos
 
-func(todos *Todos) add (title string) {
+func(todos *Todos) add(title string) {
 
     todo := Todo{
         Title : title,
@@ -29,6 +29,26 @@ func(todos *Todos) add (title string) {
 
     *todos = append(*todos, todo)
 }
+
+func(todos *Todos) validateIndex(index int) error {
+
+    if index < 0 || index >= len(*todos){
+        return errors.New("invalid index")
+    }
+    return nil
+}
+
+func(todos *Todos) delete(index int) error {
+
+    if err := todos.validateIndex(index); err != nil {
+        return err 
+    }
+
+    *todos = append((*todos)[:index], (*todos)[index+1:]...)
+    return nil
+}
+
+
 
 
 
